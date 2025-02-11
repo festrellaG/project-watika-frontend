fd18facea33c7dd0fbd381ff8c2edb2c4b116f47
Fabian Eduardo Estrella Romero <festrella@aeromexico.com>
2025-02-11 17:36:30 -0600

    add space in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="page">
       <Header />
-
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/booking" element={<Booking />} />
